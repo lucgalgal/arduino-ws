ef9d991a52ecc8172cdef3a4608c669db8a374a2
Lucía <luciagalpgal@hotmail.com>
2019-04-01 20:48:11 +0200

    Created fuction to measure temp: Doesn't work yet

--- Practica2/Practica2.ino
+++ Practica2/Practica2.ino
@@ -2,6 +2,7 @@
 #include <Wire.h>
 
 /* Single-pass function to configure the app */
+uint16_t FuncionTemperatura();
 void setup()
 {
   /* Join i2c bus (address optional for master) */
@@ -17,15 +18,16 @@ void loop()
   /* Welcome message! Useful to know where  */
   Serial.printf("Ahoy! ESP8266 here!\n---\n");
 
-  /* Request data from slave with address 0x76 */
-  Wire.requestFrom(0x76, 1);
+  
 
   /*For reading I have to use the information given on the arduino documentation*/
   
   Wire.beginTransmission(0x76);                     
   Wire.write(0xD0);              
-  Wire.endTransmission();     
-
+  Wire.endTransmission();
+       
+/* Request data from slave with address 0x76 */
+  Wire.requestFrom(0x76, 1);
 
   /* Wait for data to be available */
   while (Wire.available())
@@ -37,6 +39,45 @@ void loop()
     Serial.printf("Received byte: %02X\n", c);
   }
 
+  /*Call the function where I will obtain the temp measure*/
+    uint16_t Temp= FuncionTemperatura();
+  /* Send it to console/monitor */
+    Serial.printf("Received Temperature: %02X\n", Temp);
+  
   /* Ensure not to flood with a huge amount of fast data */
   delay(500);
+}
+
+uint16_t FuncionTemperatura(){
+ /*Declaring the variables where I'll store the temp value*/
+    uint8_t MostSignificantB=0;
+    uint8_t LeastSignificantB=0;
+  
+ /*Reading from the slave*/
+  Wire.beginTransmission(0x76);                     
+ /*I write the direction from which I will read */
+  Wire.write(0xFA);              
+  Wire.endTransmission();
+
+  /* Request data from slave with address 0x76, in this case, reading the Temp I have two bytes in which I obtain the temp */
+  Wire.requestFrom(0x76, 2);
+
+
+  /* Wait for data to be available */
+  while (Wire.available())
+  {
+    /* Receive the byte */
+    uint8_t MostSignificantB = Wire.read();
+    uint8_t LeastSignificantB = Wire.read();
+    
+    /* Send it to console/monitor */
+   Serial.printf("Received MOST: %02X\n", MostSignificantB);
+   Serial.printf("Received LEAST: %02X\n", LeastSignificantB);
+  }
+
+  return (MostSignificantB << 8) | LeastSignificantB;
+
+
+
+
 }
