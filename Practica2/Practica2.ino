ec2852431e6712fb80b22082037dbc0aca79a928
Lucía <luciagalpgal@hotmail.com>
2019-04-04 18:29:01 +0200

    Practica 2 Working!

--- Practica2/Practica2.ino
+++ Practica2/Practica2.ino
@@ -1,6 +1,10 @@
 /* Include required headers and/or libraries */
 #include <Wire.h>
 
+#define SLAVE_ADDRESS 0x76
+#define BLOCK_FIRST_ADDRESS 0x88
+#define BLOCK_LENGTH 118
+
 /* Single-pass function to configure the app */
 uint16_t FuncionTemperatura();
 void setup()
@@ -22,30 +26,65 @@ void loop()
 
   /*For reading I have to use the information given on the arduino documentation*/
   
-  Wire.beginTransmission(0x76);                     
-  Wire.write(0xD0);              
+  Wire.beginTransmission(SLAVE_ADDRESS);                     
+  Wire.write(BLOCK_FIRST_ADDRESS);              
   Wire.endTransmission();
+
+  /* Use static to prevent block re-allocation on each pass */
+  /*             NAME    LENGTH OF THE ARRAY  VALUE*/
+  static uint8_t memory_map[BLOCK_LENGTH] = { 0x00 };
+  
+  /* Array index, stores position to write a received byte */
+  uint8_t memory_idx = 0;
+
+  /* Used to check the received amount */
+  uint8_t rx_bytes = 0;
+
        
-/* Request data from slave with address 0x76 */
-  Wire.requestFrom(0x76, 1);
+  /* Request data from slave with address 0x76 */
+  Wire.requestFrom(SLAVE_ADDRESS, BLOCK_LENGTH);
 
   /* Wait for data to be available */
-  while (Wire.available())
+  while (rx_bytes == 0)
   {
-    /* Receive the byte */
-    uint8_t c = Wire.read();
-
-    /* Send it to console/monitor */
-    Serial.printf("Received byte: %02X\n", c);
+    rx_bytes=Wire.available();
   }
 
-  /*Call the function where I will obtain the temp measure*/
-    uint16_t Temp= FuncionTemperatura();
-  /* Send it to console/monitor */
-    Serial.printf("Received Temperature: %02X\n", Temp);
-  
+  /*Whenever a data is available store it in the array*/
+  for(memory_idx = 0; memory_idx < rx_bytes; memory_idx++)
+  {
+    memory_map[memory_idx]=Wire.read();
+  }
+
+  /* Print the block */
+  Serial.printf(" ++ | 00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F\n");
+  Serial.printf("----+------------------------------------------------");
+  for (memory_idx = 0; memory_idx < rx_bytes; memory_idx++)
+  {
+    /* Create a new line each 16 printed bytes */
+    if ((memory_idx % 16) == 0)
+    {
+      Serial.printf("\n %02X |", memory_idx + BLOCK_FIRST_ADDRESS);
+    }
+
+    /* Print a byte */
+    Serial.printf(" %02X", memory_map[memory_idx]);
+  }
+  Serial.printf("\nEnd of data\n");
+
   /* Ensure not to flood with a huge amount of fast data */
   delay(500);
+
+  
+
+   
+  /*Call the function where I will obtain the temp measure*/
+   // uint16_t Temp= FuncionTemperatura();
+  /* Send it to console/monitor */
+   // Serial.printf("Received Temperature: %02X\n", Temp);
+  
+  /* Ensure not to flood with a huge amount of fast data */
+  // delay(500);
 }
 
 uint16_t FuncionTemperatura(){
@@ -60,6 +99,8 @@ uint16_t FuncionTemperatura(){
   Wire.endTransmission();
 
   /* Request data from slave with address 0x76, in this case, reading the Temp I have two bytes in which I obtain the temp */
+
+  /*Right now, I have to read 118 bytes, all the registers from the BME280*/
   Wire.requestFrom(0x76, 2);
 
 
