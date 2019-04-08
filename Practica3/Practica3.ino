06c5702aa02726d3231f58e4813dd19996c873ef
Lucía <luciagalpgal@hotmail.com>
2019-04-08 20:31:23 +0200

    File attempt

--- Practica3/Practica3.ino
+++ Practica3/Practica3.ino
@@ -1,17 +1,43 @@
-/* Include required headers and/or libraries */
+/* Include required headers and/or libraries */ 
+#include <Wire.h> 
+#include "SparkFunBME280.h" 
+/*For the file*/
 #include <FS.h>
-
 #define TESTFILE "/test_file.txt"
+#define MY_STR_LEN 1024
+/*For the sensor*/
+#define SLAVE_ADDRESS 0x76 
+/* Instantiate a BME280 object called BME280_obj */ 
+BME280 BME280_obj; 
 
-/*
- * Single-pass function to configure the app
- */
-void setup()
-{
-  /* Start serial for output */
-  Serial.begin(115200);
-
-  /* Initialize the file system */
+/*Prototype of the fuctions I'll use*/
+ data BMfuction ();
+ void store (data sensor);
+ 
+/* 
+ * Single-pass function to configure the app 
+ */ 
+void setup() 
+{ 
+  /* Start serial for output */ 
+  Serial.begin(115200); 
+ 
+  /* Join I2C bus and set it to 400 kHz */ 
+  Wire.begin(0, 2); 
+  Wire.setClock(400000); 
+ 
+  /* Address the sensor */ 
+  BME280_obj.setI2CAddress(SLAVE_ADDRESS); 
+ 
+  /* Check communication before continue */ 
+  if (BME280_obj.beginI2C(Wire) == false) 
+  { 
+    Serial.printf("The sensor did not respond. Please check wiring.\n"); 
+ 
+    /* Stop here (WDT will reset at some point) */ 
+    while(1); 
+  } 
+   /* Initialize the file system */
   Serial.printf("Initializing SPIFFS\n");
   if (SPIFFS.begin() == false)
   {
@@ -20,18 +46,81 @@ void setup()
     /* Stop here (WDT will reset at some point) */
     while(1) {};
   }
+} 
+ 
+/* 
+ * Recurrent task, called forever 
+ */ 
+void loop() 
+{ 
+   struct values
+  {
+    uint16_t H;
+    uint16_t P;
+    uint16_t A; 
+    uint16_t T;    
+  };
+
+  struct values sensor;
+  /* Welcome message! Useful as a control point */ 
+  Serial.printf("Ahoy! ESP8266 here!\n---\n"); 
+  
+  sensor= data BMfuction ();
+   
+ 
+  /* Ensure not to flood with a huge amount of fast data */ 
+  delay(500); 
+} 
+
+data BMfuction ()
+{
+/*Declare all variables I'll need*/ 
+
+struct data 
+{
+  uint16_t H;
+  uint16_t P;
+  uint16_t A; 
+  uint16_t T;    
+};
+
+struct data sensor;
+   
+  /*Read all sensors */ 
+  sensor.H = BME280_obj.readFloatHumidity(); 
+  Serial.printf("Humidity: %f \n",H); 
+   
+ 
+  sensor.P = BME280_obj.readFloatPressure(); 
+  Serial.printf(" Pressure: %f \n",P); 
+   
+  sensor.A = BME280_obj.readFloatAltitudeMeters(); 
+  Serial.printf(" Alt: %f \n",A); 
+   
+  //Serial.print(BME280_obj.readFloatAltitudeFeet(), 1); 
+   
+  sensor.T = BME280_obj.readTempC(); 
+  Serial.printf(" Temp: %f \n",T); 
+   
+  //Serial.printf(BME280_obj.readTempF(), 2); 
+ 
+  Serial.printf("\n"); 
+
+  return sensor;
 }
 
-/*
- * Recurrent task, called forever
- */
-void loop()
+void store (data sensor)
 {
-  /* Welcome message! Useful as a control point */
-  Serial.printf("Ahoy! ESP8266 here!\n---\n");
+  struct data 
+  {
+  uint16_t H;
+  uint16_t P;
+  uint16_t A; 
+  uint16_t T;    
+  };
 
   File test_file;
-  #define MY_STR_LEN 1024
+  
   uint8_t my_string[MY_STR_LEN];
   uint16_t my_line = 0;
 
@@ -62,9 +151,12 @@ void loop()
 
     /* Opened, now put some (at the end of the file) */
     Serial.printf("Filling file '" TESTFILE "' with some data\n");
-    test_file.printf("This is the first line\n");
-    test_file.printf("This is the second line\n");
-    test_file.printf("This is the third line\n");
+    test_file.printf("Data Humidity: %d", sensor.H);
+    test_file.printf("Data Pressure: %d", sensor.P);
+    test_file.printf("Data Height: %d", sensor.A);
+    test_file.printf("Data Temperature: %d", sensor.T);
+    test_file.close();
+    
     test_file.close();
 
     /* Mode 'r' create if not exists:
@@ -98,4 +190,5 @@ void loop()
     /* Ensure other tasks are working (avoid WDT reset) */
     yield();
   }
+
 }
